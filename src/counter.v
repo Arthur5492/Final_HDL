10d3dac334fbc4fa862e097de7ef826a97b9a594
arthur5492 <arthurofr@gmail.com>
2024-07-05 10:31:22 -0300

    Ainda falta terminar

--- src/counter.v
+++ src/counter.v
@@ -1,45 +1,39 @@
-//Falta terminar
-
 module counter
 #(parameter size = 4)
 (
-	clk,
-	r,
-	e,
-	tc,
-	data,
-	count
+    clk,
+	 r,
+	 e,
+	 data,
+	 tc,
+	 count
 );
-
-
 
 input wire clk;
 input wire r;
 input wire e;
 input wire [size-1:0] data;
-
 output reg tc;
 output reg [size-1:0] count;
 
-always@(posedge r) begin
-	
+always @(posedge clk or posedge r) begin
+    if (r == 1'b1) begin
+        count <= {size{1'b0}}; 
+        tc <= 1'b0;
+    end
+    else begin
+		if (e == 1'b1) begin
+        if (count == size) begin 
+            count <= {size{1'b0}}; 
+            tc <= 1'b1; 
+        end
+        else begin
+            count <= count + 1'b1; 
+            tc <= 1'b0;
+        end
+		end
+    end
 end
 
-always@(posedge clk, posedge r)begin	
-	if(r == 1'b1)begin
-		count <= 4'b0000;
-		tc <= 1'b0;
-	end
-	else begin
-		if (e == 1'b1) begin
-			tc <= 1'b0;
-			count <= count + 1'b1;
-		end
-		else if(count == data) begin
-			count <=1'b0;
-			tc <= 1'b1;
-		end
-	end
-end
 
 endmodule
